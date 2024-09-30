224d33e9d505aa00a06a1be9ed41830eef9a2dd1
ishika vishwakarma <132936647+ishikavishwakarma@users.noreply.github.com>
2024-09-30 12:44:14 +0530

    make a well structure and claim

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import Hero from './HomeSubComponents/Hero'
+import Products from './HomeSubComponents/Products'
+import Partners from './HomeSubComponents/Partners'
+import Exam from './HomeSubComponents/Exam'
+
+import Workflow from './HomeSubComponents/Workflow'
+import Contact2 from './HomeSubComponents/Contact2'
+
+
+
+const Home = () => {
+  return (
+    <div>
+
+      <Hero />
+      <Products />
+      <Partners />
+      <Exam />
+      <Workflow />
+
+      <Contact2 />
+
+      
+      
+    </div>
+  )
+}
+
+export default Home
