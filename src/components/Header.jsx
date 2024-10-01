7411db8dfd47afcb30feb1ffc8b8996191c1081d
ishika vishwakarma <132936647+ishikavishwakarma@users.noreply.github.com>
2024-10-01 13:11:45 +0530

    navbar and tables

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -111,11 +111,11 @@ function NavListMenu() {
           </MenuItem>
 
           {sublink.map((sublinkItem, sublinkKey) => (
-              <Link to={sublinkItem.link} key={sublinkKey} className="text-sm text-blue-gray-500">
+              <a href={sublinkItem.link} key={sublinkKey} className="text-sm text-blue-gray-500">
             <MenuItem  className="flex items-center cursor-pointer gap-3 rounded-lg">
                 {sublinkItem.name}
             </MenuItem>
-              </Link>
+              </a>
           ))}
 
         </div>
@@ -181,9 +181,9 @@ function NavList() {
         color="blue-gray"
         className="font-medium"
       >
-        <Link to="/" >
+        <a href="/" >
         <ListItem className="flex items-center text-base gap-2 py-2 pr-4">Home</ListItem>
-        </Link>
+        </a>
       </Typography>
       <NavListMenu />
       <Typography
@@ -193,10 +193,10 @@ function NavList() {
         color="blue-gray"
         className="font-medium"
       >
-         <Link to="/aboutUs" >
+         <a href="/aboutUs" >
 
         <ListItem className="flex items-center text-base gap-2 py-2 pr-4">About Us</ListItem>
-         </Link>
+         </a>
       </Typography>
       <Typography
         as="a"
@@ -205,9 +205,9 @@ function NavList() {
         color="blue-gray"
         className="font-medium"
       >
-         <Link to="/ourPartner" >
+         <a href="/ourPartner" >
         <ListItem className="flex items-center text-base gap-2 py-2 pr-4">Our Partner</ListItem>
-         </Link>
+         </a>
       </Typography>
       <Typography
         as="a"
@@ -216,11 +216,11 @@ function NavList() {
         color="blue-gray"
         className="font-medium"
       >
-        <Link to="/claim" >
+        <a href="/claim" >
         <ListItem className="flex items-center text-base gap-2 py-2 pr-4">
           Claim
         </ListItem>
-        </Link>
+        </a>
       </Typography>
       <Typography
         as="a"
@@ -229,11 +229,11 @@ function NavList() {
         color="blue-gray"
         className="font-medium"
       >
-        <Link to="/contact" >
+        <a href="/contact" >
         <ListItem className="flex items-center text-base gap-2 py-2 pr-4">
           Contact Us
         </ListItem>
-        </Link>
+        </a>
       </Typography>
     </List>
   );
@@ -248,75 +248,7 @@ const Header = () => {
     );
   }, []);
   return (
-    // <div>
-    //     <header className='font-[sans-serif] min-h-[60px] tracking-wide relative z-50 '>
-
-    //         <section className="bg-[#ffffff] min-h-[40px] px-4 py-4 border border-gray-500 sm:px-10 flex items-center max-sm:hidden">
-    //             <button type="button" className="text-gray-800 text-sm">
-    //                 <p className='flex items-center gap-2 text-sm '><span className='text-blue-700'><MdLocationPin /></span>3 floor lalwani complex, above Axis Bank, Vidya Nagar, Bhopal, Madhya Pradesh 462026</p>
-    //             </button>
-    //             <span className="border-l h-3 mx-6 max-sm:hidden"></span>
-    //             <button type="button" className="text-gray-800 text-sm max-sm:my-2">
-    //                 <a className='flex items-center gap-2 text-sm ' href="mailto:noreplay@notion.com"><span className='text-blue-700'><MdEmail /></span>noreplay@notion.com</a>
-    //             </button>
-    //             <div className="sm:ml-auto text-gray-800">
-    //                 <div>
-    //                     <span className='flex items-center gap-3 text-xl text-gray-800'><TiSocialFacebook /><TiSocialTwitter /><TiSocialPinterest /><TiSocialYoutube /></span>
-    //                 </div>
-    //             </div>
-    //         </section>
-
-    //         <div className='flex items-center justify-between pr-2 h-24 bg-[#7166F5] lg:gap-y-4 gap-y-6 gap-x-4'>
-    //             <div className='logowrap w-72 max-sm:bg-none'>
-    //                 <div className='bg-white logobg'>
-    //                     <a className='px-5' href="/"><img src="assets/images/header/logo.png" alt="logo" className='w-40' />
-    //                     </a>
-    //                 </div>
-    //             </div>
-
-    //             <div className='hidden max-sm:block'>
-    //                 <svg width="30px" height="30px" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" fill="none">
-    //                     <path fill="#000000" fillRule="evenodd" d="M18 5a1 1 0 100-2H2a1 1 0 000 2h16zm-4 4a1 1 0 100-2H6a1 1 0 100 2h8zm5 3a1 1 0 01-1 1H2a1 1 0 110-2h16a1 1 0 011 1zm-5 5a1 1 0 100-2H6a1 1 0 100 2h8z" />
-    //                 </svg>
-    //             </div>
-    //             <div id="collapseMenu"
-    //                 className="max-lg:hidden lg:!flex lg:items-center max-lg:before:fixed max-lg:before:bg-black max-lg:before:opacity-40 max-lg:before:inset-0 max-lg:before:z-50">
-    //                 <ul
-    //                     className='lg:!flex lg:gap-x-10 max-lg:space-y-3 max-lg:fixed max-lg:bg-[#151d20] max-lg:w-2/3 max-lg:min-w-[300px] max-lg:top-0 max-lg:left-0 max-lg:px-10 max-lg:py-4 max-lg:h-full max-lg:shadow-md max-lg:overflow-auto z-50'>
-    //                     <li className='mb-6 hidden max-lg:block'>
-    //                         <a href="/"><img src="assets/images/header/logo.png" alt="logo"
-    //                             className='w-36' />
-    //                         </a>
-    //                     </li>
-    //                     <li
-    //                         className='max-lg:border-b max-lg:py-3 relative lg:after:absolute lg:after:bg-white lg:after:w-full lg:after:h-[2px] lg:after:block lg:after:-bottom-4 lg:after:transition-all lg:after:duration-300'>
-    //                         <a href='/' className='text-white block text-[15px] font-medium'>Home</a>
-    //                     </li>
-    //                     <li
-    //                         className='max-lg:border-b max-lg:py-3 relative lg:hover:after:absolute lg:after:bg-white lg:after:w-0 lg:hover:after:w-full lg:hover:after:h-[2px] lg:after:block lg:after:-bottom-4 lg:after:transition-all lg:after:duration-300'>
-    //                         <a href='/' className='text-white block text-[15px] font-medium'>About Us</a>
-    //                     </li>
-    //                     <li
-    //                         className='max-lg:border-b max-lg:py-3 relative lg:hover:after:absolute lg:after:bg-white lg:after:w-0 lg:hover:after:w-full lg:hover:after:h-[2px] lg:after:block lg:after:-bottom-4 lg:after:transition-all lg:after:duration-300'>
-    //                         <a href='/' className='text-white block text-[15px] font-medium'>Our Partner </a>
-    //                     </li>
-    //                     <li
-    //                         className='max-lg:border-b max-lg:py-3 relative lg:hover:after:absolute lg:after:bg-white lg:after:w-0 lg:hover:after:w-full lg:hover:after:h-[2px] lg:after:block lg:after:-bottom-4 lg:after:transition-all lg:after:duration-300'>
-    //                         <a href='/' className='text-white block text-[15px] font-medium'>Contact Us</a>
-    //                     </li>
-    //                 </ul>
-    //             </div>
-    //             <div className='flex items-center justify-end gap-5 px-5 w-[25vw] h-full max-sm:bg-none max-sm:hidden buttonbg'>
-    //                 <button className="rounded-md relative flex px-3 py-1.5 items-center justify-center overflow-hidden bg-[#7166F5] hover:text-black text-white shadow-2xl transition-all before:absolute before:-bottom-10 before:h-10 before:w-10 before:rounded-full before:bg-white before:duration-500 before:ease-out hover:before:h-56 hover:before:w-56">
-    //                     <span className="relative z-10">Become A POSP</span>
-    //                 </button>
-    //                 <button className="rounded-md relative flex px-3 py-1.5 items-center justify-center overflow-hidden bg-[#7166F5] hover:text-black text-white shadow-2xl transition-all before:absolute before:-bottom-10 before:h-10 before:w-10 before:rounded-full before:bg-white before:duration-500 before:ease-out hover:before:h-56 hover:before:w-56">
-    //                     <span className="relative z-10">Login</span>
-    //                 </button>
-    //             </div>
-    //         </div>
-    //     </header>
-    // </div>
+    
     <>
       <section className="backdrop-saturate-100 border-b-gray-300 border-b min-h-[40px] hidden px-4 py-2 sm:px-10 md:flex items-center max-sm:hidden">
         <button type="button" className="text-gray-800 text-xs">
