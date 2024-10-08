d3ef9102e93d1f2b84ff85ec2ad40cc754dd2d25
Surendra Kumar <88983549+Surendra360@users.noreply.github.com>
2024-10-08 18:30:41 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,8 @@ import Footer from './components/footer'
 
 const App = () => {
   return (
-// <<<<<<< surendra/feature
+
     <div className='bg-[#e0f1fb60] font-roboto'>
-// =======
-//     <div  className="font-roboto">
-// >>>>>>> master
     
     <Header />
     <Routing/>
