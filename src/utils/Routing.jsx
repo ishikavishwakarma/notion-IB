3372457eea2159ecd7f527576d46165ec83a0cdb
ishika vishwakarma <132936647+ishikavishwakarma@users.noreply.github.com>
2024-11-15 15:23:43 +0530

    navigate

--- src/utils/Routing.jsx
+++ src/utils/Routing.jsx
@@ -119,8 +119,8 @@ const Routing = () => {
         <Route path="/terms-conditions" element={<TermAndConditions />} />
         <Route path="/privacy" element={<Privacy />} />
         <Route path="*" element={<Home/>} />
-        <Route path="/javascript" element={<Home/>} />
-        <Route path="/js" element={<Home/>} />
+        <Route path="/javascript" element={<Navigate to="/" replace />} />
+        <Route path="/js" element={<Navigate to="/" replace />} />
 
       </Routes>
     </div>
